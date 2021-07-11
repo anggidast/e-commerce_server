15dc5df8c9d223c976420e3ddd73427702dbd549
anggidast <adastariana@gmail.com>
2021-07-11 23:05:55 +0700

    fix carts routes

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.use(fileUpload());
 app.use(routes);
 app.use(errorHandler);
 
-// if (process.env.NODE_ENV == 'test') module.exports = app;
-// else app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
-app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
+if (process.env.NODE_ENV == 'test') module.exports = app;
+else app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
+// app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
--- routes/cart.js
+++ routes/cart.js
@@ -5,13 +5,18 @@ const { Product, User, ShoppingCart } = require('../models');
 router.use(authentication);
 
 router.post('/:id', (req, res, next) => {
+  let newCart;
   ShoppingCart.create({
     amount: req.body.amount,
     ProductId: req.params.id,
     UserId: req.UserId,
   })
     .then((result) => {
-      res.status(201).json({ message: 'created', data: result });
+      newCart = result;
+      return Product.decrement('stock', { by: result.amount, where: { id: result.ProductId } });
+    })
+    .then(() => {
+      res.status(201).json({ message: 'created', data: newCart });
     })
     .catch((err) => next(err));
 });
@@ -19,7 +24,6 @@ router.post('/:id', (req, res, next) => {
 router.get('/', (req, res, next) => {
   ShoppingCart.findAll({ attributes: ['id', 'amount', 'UserId', 'createdAt', 'updatedAt'], include: [Product], where: { UserId: req.UserId } })
     .then((result) => {
-      console.log(result);
       res.status(200).json({ success: true, data: result });
     })
     .catch((err) => next(err));
@@ -30,9 +34,12 @@ router.delete('/:id', cartsAuthorization, (req, res, next) => {
   cart
     .destroy()
     .then(() => {
+      return Product.increment('stock', { by: cart.amount, where: { id: cart.ProductId } });
+    })
+    .then(() => {
       res.status(200).json({
         message: 'deleted',
-        deletedData: req.cart,
+        deletedData: cart,
       });
     })
     .catch((err) => next(err));
@@ -40,6 +47,8 @@ router.delete('/:id', cartsAuthorization, (req, res, next) => {
 
 router.put('/:id', cartsAuthorization, (req, res, next) => {
   const { cart } = req;
+  const oldAmount = cart.amount;
+  let updatedProduct;
 
   Object.keys(req.body).forEach((key) => {
     if (cart[key]) cart[key] = req.body[key];
@@ -47,7 +56,13 @@ router.put('/:id', cartsAuthorization, (req, res, next) => {
 
   cart
     .save()
-    .then((updatedProduct) => res.status(200).json({ success: true, data: updatedProduct }))
+    .then((result) => {
+      updatedProduct = result;
+      return Product.decrement('stock', { by: result.amount - oldAmount, where: { id: result.ProductId } });
+    })
+    .then(() => {
+      res.status(200).json({ success: true, data: updatedProduct });
+    })
     .catch((err) => next(err));
 });
 
