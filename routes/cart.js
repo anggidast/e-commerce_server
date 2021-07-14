a46dcff4c776d0c256ec134405deb36083752db2
anggidast <adastariana@gmail.com>
2021-07-14 20:05:06 +0700

    fix cart routes

--- routes/cart.js
+++ routes/cart.js
@@ -5,17 +5,17 @@ const { Product, User, ShoppingCart } = require('../models');
 router.use(authentication);
 
 router.post('/:id', (req, res, next) => {
-  let newCart;
+  // let newCart;
   ShoppingCart.create({
     amount: req.body.amount,
     ProductId: req.params.id,
     UserId: req.UserId,
   })
-    .then((result) => {
-      newCart = result;
-      return Product.decrement('stock', { by: result.amount, where: { id: result.ProductId } });
-    })
-    .then(() => {
+    // .then((result) => {
+    //   newCart = result;
+    //   return Product.decrement('stock', { by: result.amount, where: { id: result.ProductId } });
+    // })
+    .then((newCart) => {
       res.status(201).json({ message: 'created', data: newCart });
     })
     .catch((err) => next(err));
@@ -33,9 +33,9 @@ router.delete('/:id', cartsAuthorization, (req, res, next) => {
   const { cart } = req;
   cart
     .destroy()
-    .then(() => {
-      return Product.increment('stock', { by: cart.amount, where: { id: cart.ProductId } });
-    })
+    // .then(() => {
+    //   return Product.increment('stock', { by: cart.amount, where: { id: cart.ProductId } });
+    // })
     .then(() => {
       res.status(200).json({
         message: 'deleted',
@@ -47,8 +47,8 @@ router.delete('/:id', cartsAuthorization, (req, res, next) => {
 
 router.put('/:id', cartsAuthorization, (req, res, next) => {
   const { cart } = req;
-  const oldAmount = cart.amount;
-  let updatedProduct;
+  // const oldAmount = cart.amount;
+  // let updatedProduct;
 
   Object.keys(req.body).forEach((key) => {
     if (cart[key]) cart[key] = req.body[key];
@@ -56,11 +56,11 @@ router.put('/:id', cartsAuthorization, (req, res, next) => {
 
   cart
     .save()
-    .then((result) => {
-      updatedProduct = result;
-      return Product.decrement('stock', { by: result.amount - oldAmount, where: { id: result.ProductId } });
-    })
-    .then(() => {
+    // .then((result) => {
+    //   updatedProduct = result;
+    //   return Product.decrement('stock', { by: result.amount - oldAmount, where: { id: result.ProductId } });
+    // })
+    .then((updatedProduct) => {
       res.status(200).json({ success: true, data: updatedProduct });
     })
     .catch((err) => next(err));
